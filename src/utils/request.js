d5bc79bd7ff7159554b4e935102e5c312757dd08
lhb1032352370 <1032352370@qq.com>
2019-12-31 21:51:32 +0800

    部署

--- src/utils/request.js
+++ src/utils/request.js
@@ -5,7 +5,7 @@ import { getToken } from '@/utils/auth'
 
 // create an axios instance
 const service = axios.create({
-  baseURL: 'http://127.0.0.1:8080/api/public/v1', // url = base url + request url
+  baseURL: 'http://118.31.2.223/api/public/v1', // url = base url + request url
   // withCredentials: true, // send cookies when cross-domain requests
   timeout: 5000 // request timeout
 })
